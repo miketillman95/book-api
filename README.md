501aa4cc07a91ed4201a4854927e04f669a8b4fa
mike <matillman95@gmail.com>
2023-03-01 11:58:02 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Book-api where users can find books and their ratings
---
+++ app/Models/Rating.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rating extends Model
+{
+    use HasFactory;
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    protected $fillable = ['book_id', 'user_id', 'rating'];
+
+}
